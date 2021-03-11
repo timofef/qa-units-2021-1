79d44f3f884b6c3eb83d58150e2b56285529edda
Timofei Makarov <macintosh1999@mail.ru>
2021-03-11 19:07:42 +0300

    some tests

--- src/utils/sortOrders.test.js
+++ src/utils/sortOrders.test.js
@@ -1,9 +1,14 @@
 import React from 'react'
-import {sortByItemCount} from './sortOrders';
+import {sortByItemCount, sortByDate} from './sortOrders';
 
 describe('sortByItemCount function', () => {
 	it('orders are null', () => {
 		const result = sortByItemCount(null, null);
+		expect(result).toEqual(0);
+	});
+
+	it('orders are not objects', () => {
+		const result = sortByItemCount(4, 5);
 		expect(result).toEqual(0);
 	});
 
@@ -20,5 +25,96 @@ describe('sortByItemCount function', () => {
 
 		expect(result).toBe(0);
 	});
+
+	it('first order count greater the second', () => {
+		const order1 = {
+			items: ['1', '2', '3']
+		}
+
+		const order2 = {
+			items: ['1']
+		}
+
+		const result = sortByItemCount(order1, order2);
+		expect(result).toEqual(1);
+	});
+
+	it('first order count less the second', () => {
+		const order1 = {
+			items: ['1']
+		}
+
+		const order2 = {
+			items: ['1', '2', '3']
+		}
+
+		const result = sortByItemCount(order1, order2);
+		expect(result).toEqual(-1);
+	});
 });
+
+describe('sortByDate function', () => {
+	it('orders are null', () => {
+		const result = sortByDate(null, null);
+		expect(result).toEqual(0);
+	});
+
+	it('orders are not objects', () => {
+		const result = sortByDate(4, 5);
+		expect(result).toEqual(0);
+	});
+
+	it('dates are null', () => {
+		const order1 = {
+			date: null
+		}
+
+		const order2 = {
+			date: null
+		}
+
+		const result = sortByDate(order1, order2);
+		expect(result).toEqual(0);
+	});
+
+	it('dates are equal', () => {
+		const order1 = {
+			date: 1
+		}
+
+		const order2 = {
+			date: 1
+		}
+
+		const result = sortByDate(order1, order2);
+		expect(result).toEqual(0);
+	});
+
+	it('order1 date is greater than order2 date', () => {
+		const order1 = {
+			date: 2
+		}
+
+		const order2 = {
+			date: 1
+		}
+
+		const result = sortByDate(order1, order2);
+		expect(result).toEqual(-1);
+	});
+
+	it('order2 date is greater than order1 date', () => {
+		const order1 = {
+			date: 1
+		}
+
+		const order2 = {
+			date: 2
+		}
+
+		const result = sortByDate(order1, order2);
+		expect(result).toEqual(1);
+	});
+});
+
 
